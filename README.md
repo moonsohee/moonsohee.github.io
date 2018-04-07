3e3816d3afb0d093352df57ea2df674def076cca
Sohee Moon <sohee980220@likelion.org>
2018-04-07 14:12:01 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# moonsohee.github.io
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+  def index
+  end
+  
+  def birth
+  end
+  
+  def instagram
+  end
+  
+  def hobby
+  end
+  
+end
